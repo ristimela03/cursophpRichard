5246c0658874db092e1195ab21b0a6ec0739a820
ristimela03 <79428176+ristimela03@users.noreply.github.com>
2021-02-21 20:21:13 -0500

    Add files via upload

--- registro.php
+++ registro.php
@@ -1,11 +1,19 @@
 <?php
-$username= $_POST["username"];
-$password= $_POST["password"];
-$email = $_POST["email"];
-
-$archivoRegistro = fopen("$username.txt","w+"),
-fwrite($archivoRegistro,$username."\n");
-fwrite($archivoRegistro,$password."\n");
-fwrite($archivoRegistro,$email."\n");
 
 ?>
+<form action="cookies2.php" method="post" style="border: 2px dotted green; text-align:center; width: 400px;">
+
+	<p>
+		Username: <input name="username1" type="text" value="<?php if(isset($_COOKIE["username1"])) { echo $_COOKIE["username1"]; } ?>">
+	</p>
+	<p>Password: <input name="password1" type="password1" value="<?php if(isset($_COOKIE["password1"])) { echo $_COOKIE["password1"]; } ?>">
+	</p>
+    Genero: 
+        <input type="checkbox" value="Hombre" name="Genero[]">hombre</input>
+        <input type="checkbox" value="Mujer" name="Genero[]">mujer</input>
+        <input type="checkbox" value="Indefinido" name="Genero[]">Indefinido</input>
+    
+	<p><input type="checkbox" name="recuerda" value= registrarme> resgistrarme
+	</p>
+	<p><input type="submit" value="continuar"></span></p>       
+</form>
